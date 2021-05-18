c7988341c2cb58796e822005c9411a536b71aee4
HuangNO1 <fh831.cp9gw@gmail.com>
2021-05-19 01:58:30 +0800

    init & add welcome activity

---
+++ app/src/main/java/com/example/toutiao/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.example.toutiao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 2000); //2秒跳轉
+    }
+
+    private static final int GOTO_MAIN_ACTIVITY = 0;
+    private Handler mHandler = new Handler(new Handler.Callback() {
+        public boolean handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case GOTO_MAIN_ACTIVITY:
+                    Intent intent = new Intent();
+                    //將原本Activity的換成MainActivity
+                    intent.setClass(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+
+                default:
+                    break;
+            }
+            return true;
+        }
+    });
+}
